282357b2931c18585be0bde401f8c114282d9809
B33r420 <tobiasrentsch1@gmail.com>
2026-02-09 13:02:12 +0100

    Update email body for ADHS appointment notification

--- termin_checker.py
+++ termin_checker.py
@@ -96,11 +96,9 @@ def send_notification(is_test=False):
     body = f"""
     {'Hallöchen' if not is_test else 'Das ist nur ein TEST :)'}
 
-    {'Es gibt gerade einen freien Termin für die ADHS-Diagnostik!' if not is_test else 'Das ist ein Probealarm ob ihr die Mail empfangt'}
+    {'Es gibt gerade einen freien Termin für die ADHS-Diagnostik! 
+    Direktlink zur Buchungsseite: https://www.terminland.de/noris-psychotherapie/online/ADHS-Abkl%c3%a4rung%20f%c3%bcr%20Kinder%20oder%20Jugendliche/default.aspx?m=39059&ll=LDUrD&dpp=LDUrD&dlgid=9&step=3&dlg=1&a2364649380=2790686455&css=1 '}
 
-# Für Kinder:
-    {'Direktlink zur Buchungsseite: https://www.terminland.de/noris-psychotherapie/online/ADHS-Abkl%c3%a4rung%20f%c3%bcr%20Kinder%20oder%20Jugendliche/default.aspx?m=39059&ll=LDUrD&dpp=LDUrD&dlgid=9&step=3&dlg=1&a2364649380=2790686455&css=1 ' if not is_test else 'Direktlink zur Buchungsseite: https://www.terminland.de/noris-psychotherapie/online/ADHS-Abkl%c3%a4rung%20f%c3%bcr%20Kinder%20oder%20Jugendliche/default.aspx?m=39059&ll=LDUrD&dpp=LDUrD&dlgid=9&step=3&dlg=1&a2364649380=2790686455&css=1 '}
-    Grüße
     Tobi :)
     """
     
